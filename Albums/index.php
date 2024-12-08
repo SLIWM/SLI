81610c361ba6238c126a169fc8eb2e158a953d5a
RijanLexterr <94050658+RijanLexterr@users.noreply.github.com>
2024-12-08 20:56:23 +0800

    Update index.php

--- Albums/index.php
+++ Albums/index.php
@@ -40,10 +40,9 @@ include 'db.php';
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="navbar-nav ms-auto py-0">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link" aria-current="page" href="#about">About</a></li>
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="../">Home</a></li>
             <li class="nav-item"><a class="nav-link" href="album.php">Albums</a></li>
-            <li class="nav-item"><a class="nav-link" href="#followus">Follow Us</a></li>
-            <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+           
           
           </ul>
         </div>
