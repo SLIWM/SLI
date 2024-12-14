7eb23a090de90163595c20c029dcda244e28e455
RijanLexterr <94050658+RijanLexterr@users.noreply.github.com>
2024-12-14 14:40:31 +0800

    Update db.php

--- connections/db.php
+++ connections/db.php
@@ -5,6 +5,10 @@ $username = 'root';     // Your MySQL username
 $password = '';         // Your MySQL password
 $dbname = 'sli';        // Database name
 
+//$username = 'u329590524_sli2009';     // Your MySQL username
+//$password = 'Coramdeo123';         // Your MySQL password
+//$dbname = 'u329590524_sli';  
+
 // Create connection
 $conn = new mysqli($host, $username, $password, $dbname);
 
